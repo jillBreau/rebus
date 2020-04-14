5494ae0a57e1c844bb9c75a1d66b3fb8fd48c5f9
jillBreau <jmbreau@mun.ca>
2020-04-13 23:20:46 -0230

    Updates

---
+++ precache-manifest.6e1db18d4fb940ae898ade9b00c9a4cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212a8e5145853c358c9b73f136569f81",
+    "url": "/rebus/index.html"
+  },
+  {
+    "revision": "8b14e8f8b69d6b407277",
+    "url": "/rebus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "305f64aca06489e34eaf",
+    "url": "/rebus/static/js/2.ca149ba1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rebus/static/js/2.ca149ba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b14e8f8b69d6b407277",
+    "url": "/rebus/static/js/main.cdcb023d.chunk.js"
+  },
+  {
+    "revision": "b7cbb56f9cf2e81b3058",
+    "url": "/rebus/static/js/runtime-main.6150ff08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rebus/static/media/logo.5d5d9eef.svg"
+  }
+]);
