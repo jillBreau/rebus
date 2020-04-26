db505bf4ab5b4a51e8f915ba3f155b46dc77be8e
jillBreau <jmbreau@mun.ca>
2020-04-25 23:56:28 -0230

    Example, PuzzleImg components. styledButton.

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import './Home.css';
-
+import Example from '../../components/Example';
 
 class Home extends React.Component {
-    render() {
-      return (
-        <div className="contentContainer">
-          <div id="intro">
-            <h1 id="question">
-              What <i>is</i> a rebus?
-            </h1>
-            <h4 id="definition">
-              A rebus is a puzzle device which combines the use of illustrated pictures with individual letters to depict words and/or phrases. Read more <a href="https://en.wikipedia.org/wiki/Rebus" title="Rebus - Wikipedia">here</a>.
-            </h4><br/> 
-                      
-          </div>
-        </div>
-      );
-    };
+  state = {
+    exampleDisplaying: false
   };
+
+  toggleExample() {
+    this.setState({exampleDisplaying: !this.state.exampleDisplaying});
+
+    var exampleButton = document.getElementById("exampleButton");
+    if (exampleButton.innerHTML === "Show an example") {
+      exampleButton.innerHTML = "Hide the example";
+    } else { 
+      exampleButton.innerHTML = "Show an example"
+    }
+  };
+  
+  render() {
+    const { exampleDisplaying } = this.state;
+    return (
+      <div className="contentContainer">
+        <div className="mainContent" id="intro">
+          <h1 id="question">
+            What <i>is</i> a rebus?
+          </h1>
+          <h3 id="definition">
+            A rebus is a puzzle device which combines the use of illustrated pictures with individual letters to depict words and/or phrases.<br/> Read more <a href="https://en.wikipedia.org/wiki/Rebus" title="Rebus - Wikipedia">here</a>.
+          </h3>
+          <button class="styledButton" id="exampleButton" onClick={() => this.toggleExample()}>Show an example</button>
+          {exampleDisplaying && <Example />}
+        </div>
+      </div>
+    );
+  };
+};
 
 export default Home;
--- src/routes/Quizzes/Quiz1.js
+++ src/routes/Quizzes/Quiz1.js
@@ -5,8 +5,8 @@ class Quiz1 extends React.Component {
   render() {
     return (
       <div className="contentContainer">
-        <div className="puzzles">
-          <h1>Quiz 1</h1>
+        <div className="mainContent" id="questions">
+          <h1 id="questionNumber">Quiz 1 - Question 1</h1>
         </div>
       </div>
     );
